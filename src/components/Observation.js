6039a5008a1eef0d5f2d918fb26e4bc67051195c
birukyemane <bir_yem@yahoo.com>
2021-01-19 23:54:09 +0200

    - render current, minimum and maximum temperatures

--- src/components/Observation.js
+++ src/components/Observation.js
@@ -1,7 +1,16 @@
+import { minTemp, maxTemp, currentTemp } from '../Utils/utils'
+
 export default function Observation({ observation }) {
+    const current = currentTemp(observation.temperatures);
+    const minimum = minTemp(observation.temperatures);
+    const maximum = maxTemp(observation.temperatures);
+
     return (
         <div>
-            point: {observation.point}
+            point: {observation.point},
+            current: {current.value},
+            minimum: {minimum.value},
+            maximum: {maximum.value}
         </div>
     );
 }
