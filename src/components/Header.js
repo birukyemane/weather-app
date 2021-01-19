3cf55dff368b206935b9eb6198f77b63f57bc9a2
birukyemane <bir_yem@yahoo.com>
2021-01-20 00:14:32 +0200

    - install momet.js and format date in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
-export default function Header(props) {
+import moment from 'moment';
+
+export default function Header({ date }) {
+    let info = moment().format(" DD.M.YYYY HH:mm ") // 19.1.2021 21:04
     return (
         <header className="App-header">
-            Weather app
+            Weather app,
+            last updated: {info}
         </header>
     );
 }
