5e64ff98f7873d56ff1452fab3b274193f036731
birukyemane <bir_yem@yahoo.com>
2021-01-19 23:53:40 +0200

    - define current, minimum and maximum functions

---
+++ src/Utils/utils.js
@@ -0,0 +1,17 @@
+import ObservationList from "../components/ObservationList";
+
+export const minTemp = tempratures => {
+    tempratures.sort( (a, b) => a.value - b.value);
+    return tempratures[0];
+}
+
+export const maxTemp = tempratures => {
+    tempratures.sort( (a, b) => b.value - a.value);
+    return tempratures[0];
+}
+
+export const currentTemp = tempratures => {
+    tempratures.sort( (a, b) => b.date - a.date);
+    return tempratures[0];
+}
+
