eb04c0362c17925038aa26833efd796e7835bfdd
birukyemane <bir_yem@yahoo.com>
2021-01-24 23:49:58 +0200

    - change the list to columnar view

--- src/components/ObservationList.js
+++ src/components/ObservationList.js
@@ -1,11 +1,26 @@
 import Observation from './Observation'
 
 const ObservationList = ({ observations }) => {
-    return (
-        <div>
-            {observations.length ?  observations.map(observation => <Observation key={observation._id} observation={observation}/>)
-            : <p>No observation to Show</p>}
+    let header =  <div className="flex-column">
+        <h2 className="flex-row">Here are temperature observations over the last 24 hours</h2>
+        <div className="flex-row margin">
+            <div className="title">Observation point</div>
+            <div className="title">Current Temperature</div>
+            <div className="title">Minimum</div>
+            <div className="title">Maximum</div>
         </div>
+    </div>;
+
+
+    observations = observations.map(observation => <Observation key={observation._id} observation={observation}/>)
+
+    return (
+        observations.length ?  
+            <div>
+                {header}
+                {observations}
+            </div>: 
+            <h3 className="flex-row margin">No observation to Show</h3>
     )
 }
 
