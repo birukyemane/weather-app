3cb4466f178d78ae16760d09a0da44f26cd3392a
birukyemane <bir_yem@yahoo.com>
2021-01-25 01:02:03 +0200

    - change headers

--- src/components/ObservationList.js
+++ src/components/ObservationList.js
@@ -4,10 +4,10 @@ const ObservationList = ({ observations }) => {
     let header =  <div className="flex-column">
         <h2 className="flex-row">Here are temperature observations over the last 24 hours</h2>
         <div className="flex-row margin">
-            <div className="title">Observation point</div>
-            <div className="title">Current Temperature</div>
-            <div className="title">Minimum</div>
-            <div className="title">Maximum</div>
+            <div className="title">Point</div>
+            <div className="title">Current</div>
+            <div className="title">Min</div>
+            <div className="title">Max</div>
         </div>
     </div>;
 
