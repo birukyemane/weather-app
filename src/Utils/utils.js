13006a04e9df9b1c0f199c23badd82078426d8c7
birukyemane <bir_yem@yahoo.com>
2021-01-25 01:28:53 +0200

    - refactor  filter to utils

--- src/Utils/utils.js
+++ src/Utils/utils.js
@@ -1,24 +1,32 @@
+import moment from 'moment'
 
-export const minTemp = tempratures => {
-    if(tempratures.length){
-        tempratures.sort( (a, b) => a.value - b.value);
-         return tempratures[0];
+export const minTemp = temperatures => {
+    if(temperatures.length){
+        temperatures.sort( (a, b) => a.value - b.value);
+         return temperatures[0];
     }
     return 'NULL';
 }
 
-export const maxTemp = tempratures => {
-    if(tempratures.length){
-        tempratures.sort( (a, b) => b.value - a.value);
-        return tempratures[0];
+export const maxTemp = temperatures => {
+    if(temperatures.length){
+        temperatures.sort( (a, b) => b.value - a.value);
+        return temperatures[0];
     }
     return 'NULL';
 }
 
-export const currentTemp = tempratures => {
-    if(tempratures.length){
-        tempratures.sort( (a, b) => new Date(b.createdAt) - new Date(a.createdAt));
-        return tempratures[0];
+export const currentTemp = temperatures => {
+    if(temperatures.length){
+        temperatures.sort( (a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+        return temperatures[0];
     }
     return 'NULL';
+}
+
+export const filterTamperatures = temperatures => {
+    return temperatures.filter((temperature) => {
+        const result = moment().subtract(24, 'hours')
+        return moment(temperature.createdAt).isAfter(result);
+    })
 }
