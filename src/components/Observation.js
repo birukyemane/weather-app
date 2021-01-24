c5c6b8890b897ca3479e1210725d20290f98ac5a
birukyemane <bir_yem@yahoo.com>
2021-01-25 01:01:23 +0200

    - show only obsrvations within 24 hours and add  some formatting

--- src/components/Observation.js
+++ src/components/Observation.js
@@ -1,40 +1,26 @@
 import { minTemp, maxTemp, currentTemp } from '../Utils/utils'
+import moment from 'moment'
 
 export default function Observation({ observation }) {
     const {point, temperatures} = observation;
-    const current = currentTemp(temperatures);
-    const minimum = minTemp(temperatures);
-    const maximum = maxTemp(temperatures);
 
-    /*
-         <div>
-            point: {observation.point},
-            current: {(current !=='NULL'? current.value: 'No Data')},
-            minimum: {(minimum !=='NULL'? minimum.value: 'No Data')},
-            maximum: {(maximum !=='NULL'? maximum.value: 'No Data')},
-        </div>
-    */
+    let filteredTemperatures = temperatures.filter((temperature) => {
+        const result = moment().subtract(24, 'hours')
+        return moment(temperature.createdAt).isAfter(result);
+    })
+
+    const current = currentTemp(filteredTemperatures);
+    const minimum = minTemp(filteredTemperatures);
+    const maximum = maxTemp(filteredTemperatures);
 
     return (
        
         <div className="flex-column">
-            <div className="flex-row">         
-                <div className="flex-row ">
-                    <div className="stat-title">Observation point :</div>
-                    <div className="stat-info">{point}</div> 
-                </div>
-                <div  className="flex-row ">
-                    <div className="stat-title"> Current temperature :</div>
-                    <div className="stat-info">{current.value} degree celcius</div>
-                </div>
-                <div  className="flex-row ">
-                    <div className="stat-title">Minimum temperature : </div>
-                    <div className="stat-info">{minimum.value} degree celcius</div>
-                </div>         
-                <div  className="flex-row ">
-                    <div className="stat-title">NMaximum temperature : </div>
-                    <div className="stat-info">{maximum.value} degree celcius</div>
-                </div>
+            <div className="flex-row margin">    
+                <div className="title info">{point}</div>
+                <div className="title info">{(current !=='NULL'? `${current.value} °c` : 'No Data')}</div>
+                <div className="title info">{(minimum !=='NULL'? `${minimum.value} °c`:  'No Data')}</div>
+                <div className="title info">{(maximum !=='NULL'? `${maximum.value} °c` :  'No Data')}</div>
             </div>               
         </div>  
     );
